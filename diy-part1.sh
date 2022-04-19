568c92a139294f38f2ad607009e694e0f2b19cf2
jnoog <85490939+jnoog@users.noreply.github.com>
2022-04-19 10:45:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+#echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
